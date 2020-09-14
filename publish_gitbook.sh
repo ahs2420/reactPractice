d94a258ac32c8b50ec4f9ad06f4e0a38d0c20419
JunYoungSeo <seazest@naver.com>
2020-09-14 18:11:10 +0900

    Update docs

---
+++ publish_gitbook.sh
@@ -0,0 +1,17 @@
+gitbook install && gitbook build
+
+git checkout gh-pages
+
+git pull origin gh-pages --rebase
+
+cp -R _book/* .
+
+git clean -fx node_module
+git clean -fx _book
+
+git add .
+git commit -a -m "Update docs"
+
+git push origin gh-pages
+
+git checkout master
